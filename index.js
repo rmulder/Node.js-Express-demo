96d51ff95c26a70db0704ee70919f2f22c6d9da9
MichalDziedzinski <michal.marcin.dziedzinski@gmail.com>
2018-10-24 08:31:48 +0200

    Handling HTTP POST Request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     { id: 1, name: 'course1'},
     { id: 2, name: 'course2'},
@@ -13,6 +15,15 @@ app.get('/', (req, res) => {
 
 app.get('/api/courses', (req, res) => {
      res.send(courses);
+});
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 });
 
 app.get('/api/courses/:id', (req, res) => {
