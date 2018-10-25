bd31d6494b79f08bb0359b1d556737f37e9eb264
MichalDziedzinski <michal.marcin.dziedzinski@gmail.com>
2018-10-25 07:03:33 +0200

    Handling HTTP DELETE request

--- index.js
+++ index.js
@@ -21,10 +21,8 @@ app.get('/api/courses', (req, res) => {
 app.post('/api/courses', (req, res) => {
 
     const { error } = validateCourse(req.body);
-    if (error){
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(result.error.details[0].message);
+    
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -35,19 +33,26 @@ app.post('/api/courses', (req, res) => {
 
 app.put('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found.');
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
 
     const { error } = validateCourse(req.body); 
 
-    if (error){
-        res.status(400).send(result.error.details[0].message);
-        return;
-    }
+    if (error) return res.status(400).send(result.error.details[0].message);
 
     // Update course
     course.name = req.body.name;
     res.send(course);
     // Return the updated course
+});
+
+app.delete('/api/courses/:id', (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
+
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+
+    res.send(course);
 });
 
 function validateCourse(course){
@@ -56,11 +61,11 @@ function validateCourse(course){
     };
 
     return Joi.validate(course, schema);
-}
+};
 
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send('The course with the given ID was not found.');
+    if (!course) return res.status(404).send('The course with the given ID was not found.');
     res.send(course);
 });
 
