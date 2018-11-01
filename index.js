853b83b2ee13c5fdd7445b9c26dd3dc7e9211626
Michał Dziedziński <michal.marcin.dziedzinski@gmail.com>
2018-11-01 21:56:36 +0100

    Debugging

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
 const config = require('config');
 const helmet = require('helmet');
 const morgan = require('morgan');
@@ -18,8 +20,11 @@ console.log('Mail Password: ' + config.get('mail.password'));
 
 if (app.get('env') === 'development') {
     app.use(morgan('tiny'));
-    console.log('Morgan enabled...');
+    startupDebugger('Morgan enabled...');
 }
+
+// Db work...
+dbDebugger('Connected to the database...')
 
 app.use(logger);
 
